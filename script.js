8b873dd40cad947dacd9e0c4d9ab1fc9dd736e40
Ulathi Ncube <swiftly.graphics@gmail.com>
2025-07-18 08:28:17 +0200

    Fix: add function findAndDeleteBook()

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ const addBookForm = document.querySelector('.book-form');
 const addBookButton = document.querySelector('.add-book');
 const submitBookButton = document.querySelector('.submit');
 
+const authorInput = addBookForm.querySelector('#author');
+const titleInput = addBookForm.querySelector('#title');
+const pagesInput = addBookForm.querySelector('#pages');
+const yearInput = addBookForm.querySelector('#year');
+const isReadInput = addBookForm.querySelector('#is-read');
+
+let tableRows, deleteButtons;
+
 const books = [];
 
 function Book(author, title, pages, isRead, year) {
@@ -16,6 +24,7 @@ function Book(author, title, pages, isRead, year) {
 
 function createBook(book) {
   const { author, title, pages, isRead, year } = book;
+
   const newBook = new Book(author, title, pages, isRead, year);
   const uuid = self.crypto.randomUUID();
   newBook.id = uuid;
@@ -56,22 +65,37 @@ function displayBooks(books) {
     const titleCell = document.createElement('td');
     const pagesCell = document.createElement('td');
     const yearCell = document.createElement('td');
+    const editsCell = document.createElement('td');
+    const button = document.createElement('button');
 
-    const { author, title, pages, isRead, year } = book;
+    button.classList.add('delete-button');
+    button.textContent = 'Delete';
+    editsCell.appendChild(button);
+
+    const { author, title, pages, isRead, year, id } = book;
     authorCell.textContent = author;
     titleCell.textContent = title;
     yearCell.textContent = year;
     pagesCell.textContent = pages;
 
+    tableRow.setAttribute('class', 'table-row');
+    tableRow.setAttribute('data-id', id);
+
     tableRow.appendChild(authorCell);
     tableRow.appendChild(titleCell);
     tableRow.appendChild(pagesCell);
     tableRow.appendChild(yearCell);
+    tableRow.appendChild(editsCell);
 
     tableBody.appendChild(tableRow);
   }
 
-  books.forEach(book => {});
+  tableRows = tableBody.querySelectorAll('.table-row');
+  deleteButtons = tableBody.querySelectorAll('.delete-button');
+
+  tableRows.forEach(tableRow => {
+    tableRow.addEventListener('click', handleTableRowClick);
+  });
 }
 
 displayBooks(books);
@@ -91,12 +115,70 @@ function hideForm() {
   }
 }
 
+function clearBooks() {
+  while (tableBody.firstChild) {
+    tableBody.removeChild(tableBody.lastChild);
+  }
+}
+
+function findAndDeleteBook(id) {
+  const indexOfBook = books.findIndex(book => book.id === id);
+
+  if (indexOfBook !== -1) {
+    // const booksCopy = [...books];
+    books.splice(indexOfBook, 1);
+    console.log('Book deleted');
+  } else {
+    console.log('No book found!');
+  }
+}
+
 // helper funcs
+
+function getFormData() {
+  const author = authorInput.value;
+  const title = titleInput.value;
+  const pages = pagesInput.value;
+  const year = yearInput.value;
+  const isRead = isReadInput.checked;
+
+  console.log({ author, title, pages, year, isRead });
+
+  createBook({ author, title, pages, year, isRead });
+}
 
 function handleSubmitBook(event) {
   event.preventDefault();
   addBookButton.classList.remove('hidden');
   addBookForm.classList.toggle('hidden');
+
+  clearBooks();
+  getFormData();
+  displayBooks(books);
+
+  console.log(tableRows);
+}
+
+function handleTableRowClick(event) {
+  console.log('click');
+  const clickedElement = event.target;
+  const clickedElementParent = event.currentTarget;
+
+  const deleteButtonsArr = Array.from(deleteButtons);
+
+  if (!deleteButtonsArr.includes(clickedElement)) {
+    return;
+  }
+
+  const id = clickedElementParent.dataset.id;
+
+  console.dir(tableRows);
+
+  findAndDeleteBook(id);
+  clearBooks();
+  displayBooks(books);
+
+  console.dir(tableRows);
 }
 
 addBookButton.addEventListener('click', handleAddButtonClick);
